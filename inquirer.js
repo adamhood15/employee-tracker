ae60c432a3bf0c32e84c7f8a73a88c02c909db07
Mr. Hood <adamhood15@gmail.com>
2023-05-15 17:15:31 -0500

    Added department, role, and employee sql files

--- inquirer.js
+++ inquirer.js
@@ -1,18 +1,16 @@
 const {prompt} = require('inquirer');
 
 //Starting Prompt
-questions = [
+firstQuestion = [
     {
         type: 'list',
         name: 'options',
         message: 'What would you like to do?',
         choices: ['View All Employees', 'View All Departments', 'View All Roles', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role']
-    },
-    {
-        type: 'input',
-        name: 'departmentName',
-        message: 'Enter the departments name.'
-    },
+    }
+];
+
+questionAddRole = [
     {
         type: 'input',
         name: 'roleName',
@@ -27,7 +25,10 @@ questions = [
         type: 'input',
         name: 'roleDepartment',
         message: "Enter the department of the role you'd like to add"
-    },
+    }
+];
+
+questionAddEmployee = [
     {
         type: 'input',
         name: 'employeeFirstName',
@@ -47,16 +48,46 @@ questions = [
         type: 'input',
         name: 'employeeManager',
         message: "Enter the manager of the employee you'd like to add"
-    },
-  
+    }
 ];
 
+questionAddDepartment = [
+    {
+        type: 'input',
+        name: 'departmentName',
+        message: 'Enter the departments name.'
+    }
+]
 
 
+//Function that takes the user response and passes it through conditional logic depending on the users choice
 async function init() {
     try {
-        const firstAnswer = await prompt(questions);
-        console.log(firstAnswer.options);
+        const firstAnswer = await prompt(firstQuestion);
+        const response = firstAnswer.options;
+
+        if (response === 'View All Employees') {
+            console.log(response);
+
+        } else if (response === 'View All Departments') {
+            console.log(response);
+
+        } else if (response === 'View All Roles') {
+            console.log(response);
+
+        } else if (response === 'Add Department') {
+            console.log(response);
+
+        } else if (response === 'Add Role') {
+            console.log(response);
+
+        } else if (response === 'Add Employee') {
+            console.log(response);
+
+        } else if (response === 'Update Employee Role') {
+            console.log(response);
+
+        }
 
     } catch (err) {
         console.log(err);
@@ -65,5 +96,8 @@ async function init() {
 
 module.exports = {
     init,
-    questions
+    firstQuestion,
+    questionAddDepartment,
+    questionAddEmployee,
+    questionAddRole
 }
